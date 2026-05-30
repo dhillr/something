7ab2ce9f99f61edf12b5cf82a43eacb1db9cac6a
dhillr <dhillonrouzeau@gmail.com>
2026-05-30 16:50:09 -0400

    mid

--- mid_worklets.js
+++ mid_worklets.js
@@ -4,7 +4,12 @@ let sin = t => Math.sin(2 * Math.PI * t);
 let sqr = t => ((t * 2) & 1) * 2 - 1;
 let tri = t => (t > .5 ? 1 - t : t) * 4 - 1;
 
-let waves = {"saw": saw, "sin": sin, "sqr": sqr, "tri": tri};
+let waves = {
+    "saw": saw, "sawtooth": saw,
+    "sin": sin, "sine": sin,
+    "sqr": sqr, "square": sqr,
+    "tri": tri, "triangle": tri
+};
 
 class SynthProcessor extends AudioWorkletProcessor {
     static get parameterDescriptors() {
@@ -16,18 +21,24 @@ class SynthProcessor extends AudioWorkletProcessor {
     constructor() {
         super();
 
+        this.isProcessing = true;
+
         this.port.onmessage = e => {
             this.wave = waves[e.data.type];
+            
+            if (e.data.isProcessing != undefined)
+                this.isProcessing = e.data.isProcessing;
         };
     }
 
     process(inputs, outputs, params) {
         for (let i = 0; i < 128; i++) {
             let t = (currentFrame + i) / sampleRate * params.frequency;
+
             outputs[0][0][i] = this.wave(t);
         }
 
-        return true;
+        return this.isProcessing;
     }
 }
 
