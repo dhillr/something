8ce4a1d478ab8dc88feb39cfe6d0ebc159473be2
dhillr <dhillonrouzeau@gmail.com>
2026-06-03 16:04:48 -0400

    audio

--- audio_worklets.js
+++ audio_worklets.js
@@ -5,14 +5,13 @@ let sqr = t => ((t * 2) & 1) * 2 - 1;
 let tri = t => (t > .5 ? 1 - t : t) * 4 - 1;
 
 // operators
-const ADD = 1, SUB = 2, MUL = 3, DIV = 4, POW = 5;
+const ADD = 0, SUB = 1, MUL = 2, DIV = 3, POW = 4;
 
 let waves = {
     "saw": saw, "sawtooth": saw,
     "sin": sin, "sine": sin,
     "sqr": sqr, "square": sqr,
-    "tri": tri, "triangle": tri,
-    "value": tri // placeholder
+    "tri": tri, "triangle": tri
 };
 
 function applyOperator(operator, input, waveValue) {
@@ -42,13 +41,40 @@ class SynthProcessor extends AudioWorkletProcessor {
         super();
 
         this.wave = tri;
-        this.waveType = undefined;
         this.isProcessing = true;
 
         this.port.onmessage = e => {
-            this.waveType = e.data.type;
-            this.wave = waves[this.waveType];
+            this.wave = waves[e.data.type];
             
+            if (e.data.isProcessing != undefined)
+                this.isProcessing = e.data.isProcessing;
+        };
+    }
+
+    process(inputs, outputs, params) {
+        for (let i = 0; i < 128; i++) {
+            let t = (currentFrame + i) / sampleRate * params.frequency;
+            outputs[0][0][i] = this.wave(t);
+        }
+
+        return this.isProcessing;
+    }
+}
+
+class ValueProcessor extends AudioWorkletProcessor {
+    static get parameterDescriptors() {
+        return [
+            { name: "value", defaultValue: 1 },
+            { name: "operator", defaultValue: -1 }
+        ];
+    }
+
+    constructor() {
+        super();
+
+        this.isProcessing = true;
+
+        this.port.onmessage = e => {
             if (e.data.isProcessing != undefined)
                 this.isProcessing = e.data.isProcessing;
         };
@@ -59,20 +85,14 @@ class SynthProcessor extends AudioWorkletProcessor {
         input = input ? input : [];
 
         for (let i = 0; i < 128; i++) {
-            let t = (currentFrame + i) / sampleRate * params.frequency;
-            let waveValue = this.wave(t);
-
-            if (this.waveType == "value")
-                waveValue = params.frequency;
-
             if (params.operator < 0)
-                outputs[0][0][i] = waveValue;
+                outputs[0][0][i] = params.value;
             else
-                outputs[0][0][i] = applyOperator(params.operator, input[i], waveValue);
+                outputs[0][0][i] = applyOperator(params.operator, input[i], params.value);
         }
 
         return this.isProcessing;
     }
 }
 
-registerProcessor('sp', SynthProcessor);
+registerProcessor('synth-processor', SynthProcessor);
